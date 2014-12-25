341ccb4ce8f9ee24a806ac3e380b635ccb997c74
AlexLiu1985CN <AlexLiu1985CN@gmail.com>
2014-12-25 10:02:31 +0800

    2

--- src/com/efun/os/ui/view/base/BasePageLayout.java
+++ src/com/efun/os/ui/view/base/BasePageLayout.java
@@ -7,13 +7,21 @@ import android.widget.LinearLayout;
 public class BasePageLayout extends LinearLayout{
 	
 	protected BaseTitleView titleView;
+	private LinearLayout.LayoutParams mLp;
 
 	public BasePageLayout(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 	
 	public BasePageLayout(Context context) {
 		super(context);
+		init();
+	}
+	
+	private void init(){
+//		setOrientation(LinearLayout.HORIZONTAL);
+		
 	}
 
 }
