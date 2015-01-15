797c966d33b24faea57575c1d7ff59ca143b53e3
AlexLiu1985CN <AlexLiu1985CN@gmail.com>
2015-01-15 10:58:45 +0800

    16

---
+++ src/com/efun/os/control/EfunLoginEventListener.java
@@ -0,0 +1,8 @@
+package com.efun.os.control;
+
+import android.content.Context;
+
+public interface EfunLoginEventListener {
+	public void loginEvent(Context paramContext, int paramInt);
+	public void registerEvent(Context paramContext, int paramInt);
+}
